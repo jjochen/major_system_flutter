11481209fd5d2e1c1f72e084cbafcd6c5385101b
Jochen Pfeiffer <git@jochen-pfeiffer.com>
2021-04-14 14:52:46 +0200

    add tests for NumberItem

---
+++ test/numbers/widgets/number_item_test.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:major_system/numbers/numbers.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:numbers_repository/numbers_repository.dart';
+
+void main() {
+  group('NumberItem', () {
+    const id = '1234';
+    const numberOfDigits = 2;
+    const value = 42;
+    final number = Number(id: id, numberOfDigits: numberOfDigits, value: value);
+
+    testWidgets('renders ListTile', (tester) async {
+      final widget = Material(
+          child: NumberItem(
+        number: number,
+        onDismissed: (direction) {},
+        onTap: () {},
+      ));
+      await tester.pumpWidget(MaterialApp(home: widget));
+      expect(find.byType(ListTile), findsOneWidget);
+    });
+  });
+}
