c3a1ece710237586cb76380d4bfeb17c2e49bd0a
Jochen Pfeiffer <git@jochen-pfeiffer.com>
2024-07-11 11:39:10 +0200

    remove firebase function linting

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -9,11 +9,42 @@
 
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+initializeApp();
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const updateMainWord = firestore
+  .document('users/{userId}/numbers/{numberId}/words/{wordId}')
+  .onUpdate(async (change, context) => {
+    try {
+      const { userId, numberId, wordId } = context.params;
+
+      const newValue = change.after.data();
+      const previousValue = change.before.data();
+      const isNewMainWord = newValue.is_main && !previousValue.is_main;
+
+      if (isNewMainWord) {
+        logger.info('Updating main word:', newValue.value);
+
+        const userDocumentRef = _firestore().collection('users').doc(userId);
+        const numberDocumentRef = userDocumentRef.collection('numbers').doc(numberId);
+        const wordsCollectionRef = numberDocumentRef.collection('words');
+
+        const batch = _firestore().batch();
+        const wordsSnapshot = await wordsCollectionRef.get();
+        wordsSnapshot.forEach(doc => {
+          if (doc.id !== wordId && doc.data().is_main) {
+            batch.update(doc.ref, { is_main: false });
+          }
+        });
+        await batch.commit();
+
+        await numberDocumentRef.update({
+          main_word: newValue.value
+        });
+      }
+    } catch (error) {            
+      logger.error('Error updating main word:', error);
+    }
+  });
